daa6fc46a93f7e83016728de2e66c40b7dc70f8a
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-03-31 23:44:06 +0300

    Update API.jsx

--- src/Components/API/API.jsx
+++ src/Components/API/API.jsx
@@ -1,22 +1,24 @@
 import axios from 'axios';
 
-const ACCESS_KEY = 'PId6GjeQ_obh_Sys5wkx9HFhQRvv3YmIj74Modod6jY'; 
-async function searchImages(query) {
-  const url = `https://api.unsplash.com/search/photos`;
+const ACCESS_KEY = 'PId6GjeQ_obh_Sys5wkx9HFhQRvv3YmIj74Modod6jY';
 
+async function searchImages(query, page, perPage) {
+  const url = 'https://api.unsplash.com/search/photos';
 
   try {
     const response = await axios.get(url, {
       params: {
         query: query,
+        page: page,
+        per_page: perPage,
       },
       headers: {
-        Authorization: `Client-ID ${ACCESS_KEY}`
-      }
+        Authorization: `Client-ID ${ACCESS_KEY}`,
+      },
     });
-    const images = (response.data.results);
-    return images; 
-    
+
+    const images = response.data.results;
+    return images;
   } catch (error) {
     console.error('Error fetching images:', error.message);
     throw new Error('Failed to fetch images');
